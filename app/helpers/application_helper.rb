5ab242883287ab0e546f54ce83d591cb6b2e2478
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-14 06:11:52 -0400

    migrate User model and Users controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title_helper(page_title = '')
+    base_title = "Simple Events"
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' - ' + base_title
+    end
+  end
 end
