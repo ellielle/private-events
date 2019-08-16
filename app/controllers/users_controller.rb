f2c97e5dfcde36d00a43447678d792b642278208
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-16 04:57:32 -0400

    complete #create and log in user after creating an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      flash[:success] = "User created"
+      redirect_to root_url
+    end
   end
 
   def show
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
