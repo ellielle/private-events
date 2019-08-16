47cfbae328c7a008cbea21e127529e2d34745b18
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-16 05:37:22 -0400

    add route for profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#new'
+  get 'profile', to: 'users#show'
   resources :users, only: [:new, :create, :show]
 end
