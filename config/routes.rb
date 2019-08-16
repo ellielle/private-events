c961b4409bb495074757af5f479793d118ea1220
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-16 07:31:12 -0400

    fix route for posting new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'signup', to: 'users#new'
-  post 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
   get 'profile', to: 'users#show'
   resources :users, only: [:new, :create, :show]
 end
