cea6298e7f4d65368d6b96a8376adfa747db678c
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-19 06:27:31 -0400

    add logout action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user.nil?
+      flash.now[:danger] = "User does not exist."
+      render :new
+    else
+      log_in(user)
+      redirect_to profile_path
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
