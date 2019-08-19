81f56105dd21fb24f11d44991868c84f62d6b02e
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-19 06:05:29 -0400

    change double quotes to single

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
   get 'profile', to: 'users#show'
-  get 'login', to: "sessions#new"
-  post 'login', to: "sessions#create"
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   resources :users, only: [:new, :create, :show]
   resources :events, only: [:create, :show, :index]
 end
