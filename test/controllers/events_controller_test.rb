45318ffe7a9021a0b14290ba5b0a189aa7d74e5a
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-19 05:32:13 -0400

    initial events controller test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -2,17 +2,16 @@ require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
   test "should get create" do
-    get events_create_url
+    get create_path
     assert_response :success
   end
 
   test "should get show" do
-    get events_show_url
-    assert_response :success
+    # TODO log_in_as user and go to their page
   end
 
   test "should get index" do
-    get events_index_url
+    get events_path
     assert_response :success
   end
 
