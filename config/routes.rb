ec4ae00c58038d81e654daa2fec9772097bdecef
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-19 05:02:28 -0400

    add routes for events pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'create', to: 'events#create'
+  get 'events(/:title)', to: 'events#show'
+  get 'events', to: 'events#index'
   root 'pages#home'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
   get 'profile', to: 'users#show'
   resources :users, only: [:new, :create, :show]
+  resources :events, only: [:create, :show, :index]
 end
