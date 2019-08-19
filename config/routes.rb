08dda0d8fef885000ca4e2a8685d7aee42245919
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-19 05:31:20 -0400

    add routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   get 'create', to: 'events#create'
   get 'events(/:title)', to: 'events#show'
   get 'events', to: 'events#index'
-  root 'pages#home'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
   get 'profile', to: 'users#show'
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
   resources :users, only: [:new, :create, :show]
   resources :events, only: [:create, :show, :index]
 end
