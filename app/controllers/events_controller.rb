473d40184eb865e440d307989e147449767a31aa
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-20 07:03:21 -0400

    add create action to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,28 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+  end
+
   def create
+    @user = current_user
+    @event = current_user.events.build(event_params)
+    if @event.save
+      flash[:success] = "Event created"
+      redirect_to profile_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def index
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :body, :date, :location)
   end
 end
