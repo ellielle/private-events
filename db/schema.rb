e311a602644a5f9ebcb8f05f6712d3a3c2e911da
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-26 07:57:03 -0400

    re-add slug column to db for friendly_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_075717) do
+ActiveRecord::Schema.define(version: 2019_08_26_112438) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_08_19_075717) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
+    t.string "slug"
+    t.index ["slug"], name: "index_events_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
