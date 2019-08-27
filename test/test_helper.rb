68880b3e5ff5d93e37a55969a9f09b5bc3716abd
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-27 05:35:11 -0400

    fix issue with #log_in_as

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,6 +23,6 @@ end
 class ActionDispatch::IntegrationTest
 
   def log_in_as(user)
-    post login_path, params: { username: user }
+    post login_path, params: { session: { username: user } }
   end
 end
