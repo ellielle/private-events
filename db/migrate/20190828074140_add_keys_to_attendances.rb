d0be0979b5f8ce6d6892fd4368a22ddc3ec4eb5f
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-28 06:43:57 -0400

    migration for attendances table

--- db/migrate/20190828074140_add_keys_to_attendances.rb
+++ db/migrate/20190828074140_add_keys_to_attendances.rb
@@ -1,6 +1,6 @@
 class AddKeysToAttendances < ActiveRecord::Migration[5.2]
   def change
     add_column :attendances, :attendee_id, :integer, index: true
-    add_column :attendances, :event_id, :integer, index: true
+    add_column :attendances, :attended_event_id, :integer, index: true
   end
 end
