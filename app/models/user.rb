ce07623c86fffe4993d1ee871926f16463075865
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-28 06:43:39 -0400

    add associations for join table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :attendances, foreign_key: :attendee_id
-  has_many :attendees, through: :attendances
+  has_many :attended_events, through: :attendances
   has_many :events, class_name: 'Event', foreign_key: :creator_id
   validates :username, presence: true
 end
