85f8cc0da91ac6d1e12c49cbc995f772c9f4b5e6
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-28 07:19:57 -0400

    finished uncompleted test for logging in

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,13 +1,24 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:ellie)
+  end
+
   test "should get create" do
     get create_path
     assert_response :success
   end
 
-  test "should get show" do
-    # TODO log_in_as user and go to their page
+  test "should get show for a logged in user, redirect if not logged in" do
+    log_in_as(@user)
+    get profile_path
+    assert_response :success
+    delete logout_path
+    follow_redirect!
+    get profile_path
+    assert_redirected_to root_path
+    assert_not flash.empty?
   end
 
   test "should get index" do
