4a7098567b6095d2de921a2f8edfb260166e9de9
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-28 04:01:05 -0400

    add foreign keys for users and events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_112438) do
+ActiveRecord::Schema.define(version: 2019_08_28_074140) do
+
+  create_table "attendances", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "attendee_id"
+    t.integer "event_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
