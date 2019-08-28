07d7c94a38fe34b6ffc888ed8b3cbe6e798cd7ea
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-28 06:43:24 -0400

    add associations for join table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
-  has_many :attendances, foreign_key: :event_id
-  has_many :attended_events, through: :attendances
+  has_many :attendances, foreign_key: :attended_event_id
+  has_many :attendees, through: :attendances
   belongs_to :creator, class_name: "User"
   validates :title, presence: true
   validates :body, presence: true
