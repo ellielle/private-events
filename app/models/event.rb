9487a2e178c10f5533994f55dbbd4b96e45c727c
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-28 04:01:34 -0400

    change associations to work with through table

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
-  has_many :creators, class_name: "User"
+  has_many :attendances, foreign_key: :event_id
+  has_many :attended_events, through: :attendances
+  belongs_to :creator, class_name: "User"
   validates :title, presence: true
   validates :body, presence: true
   validates :date, presence: true
