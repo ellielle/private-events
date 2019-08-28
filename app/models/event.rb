821fc9a1f6facbce472713c2f1867b4804b4926b
ellielle <40385743+ellielle@users.noreply.github.com>
2019-08-28 07:12:54 -0400

    fix associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   has_many :attendances, foreign_key: :attended_event_id
-  has_many :attendees, through: :attendances
+  has_many :attendees, through: :attendances, source: :attendee
   belongs_to :creator, class_name: "User"
   validates :title, presence: true
   validates :body, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :attendances, foreign_key: :attendee_id
-  has_many :attended_events, through: :attendances
+  has_many :attended_events, through: :attendances, source: :attended_event
   has_many :events, class_name: 'Event', foreign_key: :creator_id
   validates :username, presence: true
 end
