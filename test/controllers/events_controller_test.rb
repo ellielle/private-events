73721dfe2ea22b569d736c02ba90201791e8e1aa
ellielle <40385743+ellielle@users.noreply.github.com>
2019-09-03 07:31:10 -0400

    change controller test due to changes in index

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -10,9 +10,15 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get index" do
+  test "should get index when logged in" do
+    log_in_as(@user)
     get events_path
     assert_response :success
+  end
+
+  test "should redirect to login page when logged out" do
+    get events_path
+    assert_redirected_to login_path
   end
 
   test "should get show" do
