889f3f211e523bfccac0c30b111b78510408eec4
ellielle <40385743+ellielle@users.noreply.github.com>
2019-09-03 05:24:26 -0400

    moved a test to events_display_test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -10,20 +10,12 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get show page for a logged in user, redirect if not logged in" do
-    log_in_as(@user)
-    get profile_path
-    assert_response :success
-    delete logout_path
-    follow_redirect!
-    get profile_path
-    assert_redirected_to root_path
-    assert_not flash.empty?
-  end
-
   test "should get index" do
     get events_path
     assert_response :success
   end
 
+  test "should get show" do
+    get event_path(events(:birthday))
+  end
 end
