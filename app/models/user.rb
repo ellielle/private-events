3e1418c9ddc7572c251f15f6d0aa9a8c60d4d476
ellielle <40385743+ellielle@users.noreply.github.com>
2019-09-05 07:05:37 -0400

    change foreign key, decided to implement differently

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :attendances, foreign_key: :attendee_id
   has_many :attended_events, through: :attendances, source: :attended_event
   has_many :events, class_name: 'Event', foreign_key: :creator_id
-  has_many :invitations, foreign_key: :invited_user_id
+  has_many :invitations, foreign_key: :invited_by_id
   has_many :invited_users, through: :invitations, source: :invited_user
   validates :username, presence: true
 end
