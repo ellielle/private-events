ea45437fceffb60755ef37c98a06f7307419e7d1
ellielle <40385743+ellielle@users.noreply.github.com>
2019-09-09 01:54:30 -0400

    fix associations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,4 @@
 class Invitation < ApplicationRecord
-  belongs_to :invited_user, class_name: "User"
+  belongs_to :invited_by, class_name: "User"
   belongs_to :invited_event, class_name: "Event"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :attended_events, through: :attendances, source: :attended_event
   has_many :events, class_name: 'Event', foreign_key: :creator_id
   has_many :invitations, foreign_key: :invited_by_id
-  has_many :invited_users, through: :invitations, source: :invited_user
+  has_many :invited_bys, through: :invitations, source: :invited_by
   validates :username, presence: true
 end
