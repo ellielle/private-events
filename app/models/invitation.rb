a9f2bdb02e4b24bd5cb4f82a39a72559fdd9262f
ellielle <40385743+ellielle@users.noreply.github.com>
2019-09-09 03:27:41 -0400

    fix associations so the invitation belongs to invited user

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,5 @@
 class Invitation < ApplicationRecord
-  belongs_to :invited_by, class_name: "User"
   belongs_to :invited_user, class_name: "User"
+  belongs_to :invited_by, class_name: "User"
   belongs_to :invited_event, class_name: "Event"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :attended_events, through: :attendances, source: :attended_event
   has_many :events, class_name: 'Event', foreign_key: :creator_id
   has_many :invitations, foreign_key: :invited_by_id
+  has_many :invitations, foreign_key: :invited_user_id
   has_many :invited_bys, through: :invitations, source: :invited_by
   has_many :invited_users, through: :invitations, source: :invited_user
   validates :username, presence: true
